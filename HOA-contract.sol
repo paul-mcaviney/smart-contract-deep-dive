61a025eb599c3ce2492bc78e07007a9faff14a58
Paul McAviney <87715498+paul-mcaviney@users.noreply.github.com>
2022-04-28 13:54:21 -0600

    Get a few functions working

--- HOA-contract.sol
+++ HOA-contract.sol
@@ -1,58 +1,44 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-// need to install with `npm install @openzeppelin/contracts`
-// More info here: https://docs.openzeppelin.com/contracts/4.x/api/access
- import "@openzeppelin/contracts/access/Ownable.sol";
+contract HomeOwnersBallot {
 
+struct President {
+    string name;
+    uint votes;
+}
 
-contract HomeOwnersBallot is Ownable {
+President[] public presidents;
 
-    address private owner;      // The address that initially deploys the contract becomes the owner
+// Add a President Candidate - onlyOwner
+function addCandidate(string memory _name) public {
+    presidents.push(President({name: _name, votes: 0}));
+}
 
-    // state variables for candidates
-    // an object that holds President Objects and each president has a vote number attached
-    // an object that holds Secretary Objects and each secretary has a vote number attached
-    // an object that holds Treasurer Objects and each treasurer has a vote number attached
-    // an object that holds Improvement Board Member Objects, each has a vote number attached
-
-    // an object that holds the first yes/no and also has a vote number attached
-    // an object that holds the second yes/no and also has a vote number attached
-
-    
-    
-    constructor() {  // might not need this with the openzeppelin import
-      owner = msg.sender;
+// Remove a Candidate - onlyOwner
+function removeCandidate(uint _index) public {
+    for (uint i = _index; i < presidents.length-1; i++){
+      presidents[i] = presidents[i+1];      
     }
+    presidents.pop();
+}
 
-    function setCandidates() public onlyOwner {
-        // takes in an object that is votable and sets the candidates, also setting votes to zero
+// Reset the President Vote Counts - onlyOwner
+function resetVoteCount() public {
+    for (uint p = 0; p < presidents.length; p++) {
+        presidents[p].votes = 0;
     }
+}
 
-    function resetVotes() public onlyOwner {
-        // takes in an object that contains the votes and sets it to zero
-    }
-
-    function addVote() public {
-        // takes in a votable object and increases the vote count by 1
-        // if voting for president, secretary, treasurer -- sender can only vote once
-        // if voting for Board Members, can vote 3 times
-    }
-
-    function getVotes() public {
-        // returns the votes of a votable object
-    }
-    
+// Add a vote to a candidate by name
+function addVote(string memory _name) public {
+    for (uint i = 0; i < presidents.length-1; i++){
+        // solidity can't compare strings directly
+        if (keccak256(abi.encodePacked(presidents[i].name)) == keccak256(abi.encodePacked(_name))) {
+            presidents[i].votes++;
+        }
+    } 
+}
 
 
 }
-
-// This is a smart contract for the Smart Contract Deep Dive article written by John Vester
-
-// will need the following things:
-//      1. Select the HOA President (single vote for multiple candidates)
-//      2. Select the HOA Secretary (single vote for multiple candidates)
-//      3. Select the HOA Treasurer (single vote for multiple candidates)
-//      4. Select the HOA Improvement Board (three votes out of a list of multiple candidates)
-//      5. Should we have a block party this year? (pick one of yes or no answers)
-//      6. Should we have a neighborhood yard sale this year? (pick one of yes or no answers)
