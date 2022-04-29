de221568600bc2ea4a1eecfaf1960f6ec6688735
Paul McAviney <87715498+paul-mcaviney@users.noreply.github.com>
2022-04-29 10:28:04 -0600

    Get things working and clean up

--- HOA-contract.sol
+++ HOA-contract.sol
@@ -1,61 +1,81 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-contract HomeOwnersBallot {
+// Download into project folder with `npm install @openzeppelin/contracts`
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-struct President {
-    string name;
-    uint votes;
-}
+contract HomeOwnersBallot is Ownable {
 
-// An array to hold the president candidates
-President[] public presidents; 
-
-constructor() {
-    presidents.push(President({name: "Bob", votes: 0}));
-    presidents.push(President({name: "Alice", votes: 0}));
-    presidents.push(President({name: "Billy", votes: 0}));
-    presidents.push(President({name: "Gertrude", votes: 0}));
-}
-
-// Add a President Candidate - onlyOwner
-function addCandidate(string memory _name) public {
-    presidents.push(President({name: _name, votes: 0}));
-}
-
-// Remove a Candidate - onlyOwner
-function removeCandidate(uint _index) public {
-    for (uint i = _index; i < presidents.length-1; i++){
-      presidents[i] = presidents[i+1];      
+    // Custom type to describe a Presidential Candidate and hold votes
+    struct Candidate {
+        string name;
+        uint256 votes;
     }
-    presidents.pop();
-}
 
-// Reset the President Vote Counts - onlyOwner
-function resetVoteCount() public {
-    for (uint p = 0; p < presidents.length; p++) {
-        presidents[p].votes = 0;
+    // Array of Presidential Candidates
+    Candidate[] public presidents;
+
+    constructor() {
+        presidents.push(Candidate({name: "Bob", votes: 0}));
+        presidents.push(Candidate({name: "Alice", votes: 0}));
+        presidents.push(Candidate({name: "Billy", votes: 0}));
+        presidents.push(Candidate({name: "Gertrude", votes: 0}));
     }
-}
 
-// Add a vote to a candidate by name
-// function addVote(string memory _name) public {
-//     for (uint i = 0; i < presidents.length-1; i++) {
-//         // solidity can't compare strings directly
-//         if (keccak256(abi.encodePacked(presidents[i].name)) == keccak256(abi.encodePacked(_name))) {
-//             presidents[i].votes += 1;
-//         }
-//     } 
-// }
+    // Add a President Candidate - onlyOwner
+    function addCandidate(string memory _name) public onlyOwner {
+        presidents.push(Candidate({name: _name, votes: 0}));
+    }
 
-// Add a vote to candidate by their index number
-function addVote(uint _index) public {
-    presidents[_index].votes++;
-}
+    // Remove a Candidate - onlyOwner
+    function removeCandidate(string memory _name) public onlyOwner {
+        uint256 _index;
+        bytes32 nameEncoded = keccak256(abi.encodePacked(_name));
 
-function getWinner() public view returns (string memory winner) {
-        uint winningVoteCount = 0;
-        for (uint i = 0; i < presidents.length; i++) {
+        // Set index number for specific candidate
+        for (uint256 i = 0; i < presidents.length; i++) {
+            if (keccak256(abi.encodePacked(presidents[i].name)) == nameEncoded) {
+                _index = i;
+            }
+        }
+
+        // shift candidate to be remove to the end of the array and the rest forward
+        for (uint256 i = _index; i < presidents.length - 1; i++) {
+            presidents[i] = presidents[i + 1];
+        }
+
+        // remove last item from array
+        presidents.pop();
+    }
+
+    // Reset the President Vote Counts - onlyOwner
+    function resetVoteCount() public onlyOwner {
+        for (uint256 p = 0; p < presidents.length; p++) {
+            presidents[p].votes = 0;
+        }
+    }
+
+    // Add a vote to a candidate by name
+    function addVoteByName(string memory _name) public {
+        // Encode name so only need to do once
+        bytes32 nameEncoded = keccak256(abi.encodePacked(_name));
+
+        for (uint256 i = 0; i < presidents.length; i++) {
+            // solidity can't compare strings directly, need to compare hash
+            if (keccak256(abi.encodePacked(presidents[i].name)) == nameEncoded) {
+                presidents[i].votes += 1;
+            }
+        }
+    }
+
+    // // Add a vote to candidate by their index number
+    // function addVoteByIndex(uint _index) public {
+    //     presidents[_index].votes++;
+    // }
+
+    function getWinner() public view returns (string memory winner) {
+        uint256 winningVoteCount = 0;
+        for (uint256 i = 0; i < presidents.length; i++) {
             if (presidents[i].votes > winningVoteCount) {
                 winningVoteCount = presidents[i].votes;
                 winner = presidents[i].name;
@@ -64,16 +84,4 @@ function getWinner() public view returns (string memory winner) {
 
         return winner;
     }
-
-
 }
-
-// This is a smart contract for the Smart Contract Deep Dive article written by John Vester
-
-// will need the following things:
-//      1. Select the HOA President (single vote for multiple candidates)
-//      2. Select the HOA Secretary (single vote for multiple candidates)
-//      3. Select the HOA Treasurer (single vote for multiple candidates)
-//      4. Select the HOA Improvement Board (three votes out of a list of multiple candidates)
-//      5. Should we have a block party this year? (pick one of yes or no answers)
-//      6. Should we have a neighborhood yard sale this year? (pick one of yes or no answers)
